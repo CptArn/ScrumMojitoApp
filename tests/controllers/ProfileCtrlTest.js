e99706c233f0be8b7f97fca444dd4781286836af
Arne Vlaeminck <arne.vlaeminck@student.odisee.be>
2016-11-18 18:25:38 +0100

    added test configs and configured travis

---
+++ tests/controllers/ProfileCtrlTest.js
@@ -0,0 +1,17 @@
+describe('ProfileCtrl', function() {
+	var $scope, $controller;
+
+	beforeEach(module('starter'));
+
+	beforeEach(inject(function(_$rootScope_, _$controller_){
+
+		$scope = _$rootScope_.$new();
+		$controller = _$controller_;
+
+		ctrl = $controller('ProfileCtrl', {$scope: $scope});
+	}));
+
+	it('checked schould be set to true', function() {
+		expect($scope.checked).toEqual(true);
+	})
+});

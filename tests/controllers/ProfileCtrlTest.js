cde434c8a331fe6a94c63f157d89a8d70945d908
System Administrator <root@MBPvanGheerwijn.local>
2016-11-20 14:27:57 +0100

    2nd try

--- tests/controllers/ProfileCtrlTest.js
+++ tests/controllers/ProfileCtrlTest.js
@@ -14,4 +14,27 @@ describe('ProfileCtrl', function() {
 	it('checked schould be set to true', function() {
 		expect($scope.checked).toEqual(true);
 	});
+	it('user should have all information', function() {
+		expect($scope.user).toEqual({name: "Arne Vlaeminck",email: "arne.vlaeminck@student.odisee.be",school: "Odisee, Gent",about: "fun guy, javascript is life"});
+		expect($scope.gender).toEqual({ male: true, female: false, trans: false });
+		expect($scope.ageSlider).toEqual({value: 18,min: 18,max: 99});
+		expect($scope.distanceSlider).toEqual({value: 0,min: 0,max: 20});
+	});
+	it('updating of user information', function() {
+		$scope.gender = { male: false, female: true, trans: false };
+		// $scope.user.email = "javascript4life@web.com";
+	    // $scope.ageSlider.value = 20;
+	    // $scope.distanceSlider.value = 2;
+		$scope.saveProfile();
+		expect($scope.gender).toEqual({ male: false, female: true, trans: false });
+		// expect($scope.user.email).toEqual("javascript4life@web.com");
+		// expect($scope.ageSlider.value).toEqual(20);
+		// expect($scope.distanceSlider.value).toEqual(2);
+	});
+	it('change location id', function() {
+		$scope.changed(3);
+		expect($scope.locationId).toEqual(3);
+	});
+
+
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,58 +1,73 @@
 angular.module('starter.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  // Form data for the login modal
-  $scope.loginData = {};
-
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
 })
 
 .controller('DashboardCtrl', function($scope, $http) {
-    $http.get('https://student-dating-test.herokuapp.com/greeting?name=Arne').success(function(data) {
-      $scope.helloWorld = data;
-      console.log(data.content);
-    }).error(function(error) {
-      $scope.helloWorld = "Sorry, something went wrong with our server";
-      console.log(error);
-  });
+
+        $http.get('https://student-dating-test.herokuapp.com/greeting?name=Arne').success(function(data) {
+          $scope.helloWorld = data;
+          console.log('data: ' + data.content);
+        }).error(function(error) {
+          $scope.helloWorld = "Sorry, something went wrong with our server";
+          console.log(error);
+      });
+
+
 })
 
+// Controller for user profile page
 .controller('ProfileCtrl', function($scope) {
-  $scope.checked = true;
+    $scope.checked = true;
+
+    // Dropdown location options
+    $scope.locations = [{id: 1, value: "Gent"}, {id: 2, value: "Kortrijk"}, {id: 3, value: "Leuven"}, {id: 4, value: "Brussel"}];
+
+    $scope.$on('$ionicView.beforeEnter', function() {
+        // Get profile information
+
+    });
+    // User info
+    $scope.user = {
+        name: "Arne Vlaeminck",
+        email: "arne.vlaeminck@student.odisee.be",
+        school: "Odisee, Gent",
+        about: "fun guy, javascript is life"
+    };
+    // Gender preferences
+    $scope.gender = { male: true, female: false, trans: false };
+    // Age slider options
+    $scope.ageSlider = {
+        value: 18, // Age offset
+        min: 18,
+        max: 99
+    };
+    // Distance slider options
+    $scope.distanceSlider = {
+        value: 0,
+        min: 0,
+        max: 20
+    };
+    // Save all profile settings
+    $scope.saveProfile = function() {
+        // Post to API
+        // console.log('saving profile changes');
+        $scope.user = $scope.user;
+        $scope.ageSlider.value = $scope.ageSlider.value;
+        $scope.distanceSlider.value = $scope.distanceSlider.value;
+        $scope.locationId = $scope.locationId;
+        $scope.gender = $scope.gender;
+        // console.log($scope.user);
+        // console.log($scope.ageSlider.value);
+        // console.log($scope.distanceSlider.value);
+        // console.log($scope.locationId);
+        // console.log($scope.gender);
+    };
+
+    // Save location when option is changed
+    $scope.changed = function(id) {
+        $scope.locationId = id;
+    };
 })
 
 .controller('MatchesCtrl', function($scope, $stateParams) {

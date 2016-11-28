816fd19b817c270169adc4596202cce9e4bce08c
Michiel Van Den Berghe <michiel.vandenberghe@student.odisee.be>
2016-11-28 11:23:23 +0100

    -testen
    Ik heb geen idee hoe we moeten testen als we altijd onze Facebook moeten
    verifiëren met access token en id

--- www/js/services.js
+++ www/js/services.js
@@ -8,20 +8,3 @@ angular.module('starter.services', [])
         return $http.post('http://studyfindr.herokuapp.com/user/' + $scope.user.id + '/update', data);
     };
 })
-.factory('facebookService', function($q) {
-    return {
-        getUserInfo: function() {
-            var deferred = $q.defer();
-            FB.api('/me', {
-                fields: 'first_name, gender, last_name, id'
-            }, function(response) {
-                if (!response || response.error) {
-                    deferred.reject('Error occured');
-                } else {
-                    deferred.resolve(response);
-                }
-            });
-            return deferred.promise;
-        }
-    };
-});

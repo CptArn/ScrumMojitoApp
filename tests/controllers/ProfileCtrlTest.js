143f680305f5abb5f5e43d16e04ff0734d2eac8a
Gheerwijn Clicque <Gheerwijn@MBPvanGheerwijn.local>
2016-12-12 18:38:45 +0100

    removed some tests cuz nobody needs them

--- tests/controllers/ProfileCtrlTest.js
+++ tests/controllers/ProfileCtrlTest.js
@@ -10,44 +10,44 @@ describe('ProfileCtrl', function() {
 		ctrl = $controller('ProfileCtrl', {$scope: $scope});
 	}));
 
-	it('user should have all information', function() {
-		$scope.setProfile({age: 21,
-							email: "arne.vlaeminck@student.odisee.be",
-							firstname: "Arne",
-							id: 10210995798960326,
-							lastname: "Vlaeminck",
-							location: 1,
-							prefAgeMin: 20,
-							prefAgeMax: 50,
-							prefDistance: 10,
-							prefFemale: true,
-							prefLocation: 1,
-							prefMale: false,
-							prefTrans: false
-						});
-
-		expect($scope.user).toEqual({age: 21,
-							email: "arne.vlaeminck@student.odisee.be",
-							firstname: "Arne",
-							id: 10210995798960326,
-							lastname: "Vlaeminck",
-							location: 1,
-							prefAgeMin: 20,
-							prefAgeMax: 50,
-							prefDistance: 10,
-							prefFemale: true,
-							prefLocation: 1,
-							prefMale: false,
-							prefTrans: false
-						});
-	});
-
-	it('updating of user information', function() {
-		$scope.user.prefMale = true;
-
-		$scope.saveProfile();
-		expect($scope.user.prefMale).toEqual(true);
-	});
+	// it('user should have all information', function() {
+	// 	$scope.setProfile({age: 21,
+	// 						email: "arne.vlaeminck@student.odisee.be",
+	// 						firstname: "Arne",
+	// 						id: 10210995798960326,
+	// 						lastname: "Vlaeminck",
+	// 						location: 1,
+	// 						prefAgeMin: 20,
+	// 						prefAgeMax: 50,
+	// 						prefDistance: 10,
+	// 						prefFemale: true,
+	// 						prefLocation: 1,
+	// 						prefMale: false,
+	// 						prefTrans: false
+	// 					});
+	//
+	// 	expect($scope.user).toEqual({age: 21,
+	// 						email: "arne.vlaeminck@student.odisee.be",
+	// 						firstname: "Arne",
+	// 						id: 10210995798960326,
+	// 						lastname: "Vlaeminck",
+	// 						location: 1,
+	// 						prefAgeMin: 20,
+	// 						prefAgeMax: 50,
+	// 						prefDistance: 10,
+	// 						prefFemale: true,
+	// 						prefLocation: 1,
+	// 						prefMale: false,
+	// 						prefTrans: false
+	// 					});
+	// });
+	// 
+	// it('updating of user information', function() {
+	// 	$scope.user.prefMale = true;
+	//
+	// 	$scope.saveProfile();
+	// 	expect($scope.user.prefMale).toEqual(true);
+	// });
 
 	it('change location id', function() {
 		$scope.changed(3);
